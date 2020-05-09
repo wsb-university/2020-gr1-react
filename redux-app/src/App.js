f6d6f397155821938ee0a58c74dfad863b0a8587
medi71 <wojciech@medynski.io>
2020-05-09 12:18:14 +0200

    Language Context

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
+const dictionary = {
+  pl: {
+    yourNick: 'Twój nick',
+    moveForward: 'Przejdź dalej',
+  },
+  en: {
+    yourNick: 'Your nick',
+    moveForward: 'Move forward',
+  },
+};
+
+const LanguageContext = createContext(dictionary.en);
+
 const Home = () => {
-  const handleChange = () => {
-    console.warn('change');
+  const [nick, setNick] = useState('');
+  const dictionary = useContext(LanguageContext);
+
+  const handleChange = (event) => {
+    setNick(event.target.value);
   };
 
   return (
     <section>
       Nick: <input onChange={handleChange} />
       <br />
+      {dictionary.yourNick}: {nick}
       <br />
-      <Link to="/chat">Przejdź dalej ></Link>
+      <br />
+      <Link to="/chat">{dictionary.moveForward} ></Link>
     </section>
   );
 };
 
 const Chat = () => {
-  return <section>Nick: ....</section>;
+  const dictionary = useContext(LanguageContext);
+  return <section>{dictionary.yourNick}: ....</section>;
 };
 
 const App = () => {
+  const [context, setContext] = useState('pl');
+
   return (
     <div>
-      <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route path="/chat" component={Chat} />
-      </BrowserRouter>
+      <LanguageContext.Provider value={dictionary[context]}>
+        <span onClick={() => setContext('pl')}>pl</span> |{' '}
+        <span onClick={() => setContext('en')}>en</span>
+        <br />
+        <br />
+        <BrowserRouter>
+          <Route exact path="/" component={Home} />
+          <Route path="/chat" component={Chat} />
+        </BrowserRouter>
+      </LanguageContext.Provider>
     </div>
   );
 };
